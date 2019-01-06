511d4efdf13d581d17c1fa0a4b9c583fc1d6fb3d
mirukk <mapyajapan@yahoo.co.jp>
2019-01-06 22:52:27 +0800

    更改

---
+++ VuejsApp1/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <mu-container>
+            <mu-card style="width: 100%; max-width: 375px; margin: 0 auto;">
+                <mu-card-header title="Myron Avatar" sub-title="sub title">
+                    <mu-avatar slot="avatar">
+                        <img src="1300354_p0.jpg">
+                    </mu-avatar>
+                </mu-card-header>
+                <mu-card-media title="Image Title" sub-title="Image Sub Title">
+                    <img src="11300_p0.jpg">
+                </mu-card-media>
+                <mu-card-title title="Content Title" sub-title="Content Title"></mu-card-title>
+                <mu-card-text>
+                    public const string FilePath = @"E:\TEST\";
+                    public const string FilePath_VersionLog = @"E:\TEST\TestLog";
+                    public const string DateBasePath = "";
+                </mu-card-text>
+                <mu-card-actions>
+                    <mu-button flat>Action 1</mu-button>
+                    <mu-button flat>Action 2</mu-button>
+                </mu-card-actions>
+            </mu-card>
+        </mu-container>
+
+    </div>
+</template>
+<script>
+
+</script>
---
+++ VuejsApp1/src/main.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import App from './App.vue';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+
+Vue.use(MuseUI);
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app');
