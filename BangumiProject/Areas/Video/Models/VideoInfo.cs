36d291fcf6acae548a006389a1abc915b49302e2
mirukk <mapyajapan@yahoo.co.jp>
2019-01-10 22:28:10 +0800

    更新

---
+++ BangumiProject/Areas/Video/Models/VideoInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangumiProject.Areas.Video.Models
+{
+    public class VideoInfo
+    {
+        [Key]
+        public int ID { get; set; }
+        public string VideoName { get; set; }
+        public string VInfo { get; set; }
+        public string Path { get; set; }
+    }
+}
