73a76064b6973c89b0a3932fe30f39ce0e205b03
Mirukk <mapyajapan@yahoo.co.jp>
2019-01-25 18:24:05 +0800

    今天的份，提交了

---
+++ BaseProject/UnselfTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseProject
+{
+    public class UnselfTest
+    {
+        public unsafe void Test()
+        {
+            int b = 23;
+            int* a = &b;
+        }
+    }
+}
