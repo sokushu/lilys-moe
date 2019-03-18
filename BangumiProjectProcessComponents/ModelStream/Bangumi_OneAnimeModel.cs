581a7ba5760e5fe68b2d817fe00b20bded516bf9
mirukk <mapyajapan@yahoo.co.jp>
2019-03-19 00:15:05 +0800

    更新一下

--- BangumiProjectProcessComponents/ModelStream/Bangumi_OneAnimeModel.cs
+++ BangumiProjectProcessComponents/ModelStream/Bangumi_OneAnimeModel.cs
@@ -1,4 +1,5 @@
-﻿using BangumiProjectDBServices.PageModels;
+﻿using BangumiProjectDBServices.Models;
+using BangumiProjectDBServices.PageModels;
 using BaseProject.Core;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,32 @@ namespace BangumiProjectProcessComponents.ModelStream
         {
 
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public override void BuildRulu()
         {
             
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public override Bangumi_OneAnime Build()
         {
             Bangumi_OneAnime ReturnBangumi_OneAnime = new Bangumi_OneAnime();
+
+            Anime Anime = (Anime)values["Anime"];
+            AnimeUserInfo animeUserInfo = (AnimeUserInfo)values["AnimeUserInfo"];
+
+            bool IsNull = animeUserInfo == null;
+
+            ReturnBangumi_OneAnime.Anime = Anime;
+            ReturnBangumi_OneAnime.UserAnimeNumber = IsNull ? 0 : animeUserInfo.NowAnimeNum;
+            ReturnBangumi_OneAnime.IsSub = !IsNull;
+
             return ReturnBangumi_OneAnime;
         }
     }
